fb46f674e5fc12cd373b48537fc07f8aa425cd5f
Ren Rizzolo <ren@jewishnews.net.au>
2020-06-16 09:15:03 +1000

    don't crash when there's no img url

--- lg-gallery-block/src/block/gallery-image.js
+++ lg-gallery-block/src/block/gallery-image.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import classnames from 'classnames';
+import classnames from "classnames";
 
 /**
  * WordPress Dependencies
@@ -25,50 +25,51 @@ const { isBlobURL } = wp.blob;
 
 class GalleryImage extends Component {
 	constructor() {
-		super( ...arguments );
+		super(...arguments);
 
-		this.onImageClick = this.onImageClick.bind( this );
-		this.onSelectCaption = this.onSelectCaption.bind( this );
-		this.onKeyDown = this.onKeyDown.bind( this );
-		this.bindContainer = this.bindContainer.bind( this );
+		this.onImageClick = this.onImageClick.bind(this);
+		this.onSelectCaption = this.onSelectCaption.bind(this);
+		this.onKeyDown = this.onKeyDown.bind(this);
+		this.bindContainer = this.bindContainer.bind(this);
 
 		this.state = {
 			captionSelected: false,
 		};
 	}
 
-	bindContainer( ref ) {
+	bindContainer(ref) {
 		this.container = ref;
 	}
 
 	onSelectCaption() {
-		if ( ! this.state.captionSelected ) {
-			this.setState( {
+		if (!this.state.captionSelected) {
+			this.setState({
 				captionSelected: true,
-			} );
+			});
 		}
 
-		if ( ! this.props.isSelected ) {
+		if (!this.props.isSelected) {
 			this.props.onSelect();
 		}
 	}
 
 	onImageClick() {
-		if ( ! this.props.isSelected ) {
+		if (!this.props.isSelected) {
 			this.props.onSelect();
 		}
 
-		if ( this.state.captionSelected ) {
-			this.setState( {
+		if (this.state.captionSelected) {
+			this.setState({
 				captionSelected: false,
-			} );
+			});
 		}
 	}
 
-	onKeyDown( event ) {
+	onKeyDown(event) {
 		if (
 			this.container === document.activeElement &&
-			this.props.isSelected && [ BACKSPACE, DELETE ].indexOf( event.keyCode ) !== -1
+			this.props.isSelected &&
+			[BACKSPACE, DELETE].indexOf(event.keyCode) !== -1
 		) {
 			event.stopPropagation();
 			event.preventDefault();
@@ -76,29 +77,42 @@ class GalleryImage extends Component {
 		}
 	}
 
-	componentDidUpdate( prevProps ) {
+	componentDidUpdate(prevProps) {
 		const { isSelected, image, url } = this.props;
-		if ( image && ! url ) {
-			this.props.setAttributes( {
+		console.log("didupdate", image);
+
+		if (image && !url) {
+			this.props.setAttributes({
 				url: image.source_url,
 				alt: image.alt_text,
-			} );
+			});
 		}
 
 		// unselect the caption so when the user selects other image and comeback
 		// the caption is not immediately selected
-		if ( this.state.captionSelected && ! isSelected && prevProps.isSelected ) {
-			this.setState( {
+		if (this.state.captionSelected && !isSelected && prevProps.isSelected) {
+			this.setState({
 				captionSelected: false,
-			} );
+			});
 		}
 	}
 
 	render() {
-		const { url, alt, id, link, isSelected, caption, onRemove, setAttributes, classes, 'aria-label': ariaLabel } = this.props;		
-		console.log('url', url);
-		
-		const img = (
+		const {
+			url,
+			alt,
+			id,
+			link,
+			isSelected,
+			caption,
+			onRemove,
+			setAttributes,
+			classes,
+			"aria-label": ariaLabel,
+		} = this.props;
+		console.log("url", url);
+
+		const img = url ? (
 			// Disable reason: Image itself is not meant to be interactive, but should
 			// direct image selection and unfocus caption fields.
 			/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
@@ -114,44 +128,53 @@ class GalleryImage extends Component {
 				/>
 				{isBlobURL(url) && <Spinner />}
 			</Fragment>
+		) : (
 			/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */
+			<p>Image unavailable</p>
 		);
 
-
-		const className = classnames( {
-			'is-selected': isSelected,
-			'is-transient': isBlobURL(url),			
-		}, classes );
+		const className = classnames(
+			{
+				"is-selected": isSelected,
+				"is-transient": isBlobURL(url),
+			},
+			classes
+		);
 
 		// Disable reason: Each block can be selected by clicking on it and we should keep the same saved markup
 		/* eslint-disable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
 		return (
-			<figure className={ className } tabIndex="-1" onKeyDown={ this.onKeyDown } ref={ this.bindContainer }>
-				{ isSelected &&
+			<figure
+				className={className}
+				tabIndex="-1"
+				onKeyDown={this.onKeyDown}
+				ref={this.bindContainer}
+			>
+				{isSelected && (
 					<div className="core-blocks-gallery-item__inline-menu">
 						<IconButton
 							icon="no-alt"
-							onClick={ onRemove }
+							onClick={onRemove}
 							className="blocks-gallery-item__remove"
-							label={ __( 'Remove Image' ) }
+							label={__("Remove Image")}
 						/>
 					</div>
-				}
-				{ img }
+				)}
+				{img}
 				{(caption && caption.length > 0) || isSelected ? (
 					<RichText
-						// format="string" 
-						value={caption} 
+						// format="string"
+						value={caption}
 						tagName="figcaption"
-						placeholder={__('Write caption…')}
+						placeholder={__("Write caption…")}
 						isSelected={this.state.captionSelected}
 						onChange={(newCaption) => setAttributes({ caption: newCaption })}
 						unstableOnFocus={this.onSelectCaption}
 						inlineToolbar
 					/>
-				)	:	null }
-				
-			{	/* { this.state.isOpen ? (
+				) : null}
+
+				{/* { this.state.isOpen ? (
 					<Modal
 						title="Edit caption"
 						onRequestClose={() => this.setState({isOpen: false})}>
@@ -175,9 +198,8 @@ class GalleryImage extends Component {
 	}
 }
 
-
 export default withSelect((select, ownProps) => {
-	const { getMedia } = select('core');
+	const { getMedia } = select("core");
 	const { id } = ownProps;
 
 	return {
